58f0c0c395fd65de7abf5a81a5bf4a42fa68109b
Nazar Semkiv <nazar.semkiv.2000@gmail.com>
2022-11-17 18:12:15 +0200

    ESLint push
    - revised code with ESLint (over 90 changes *-*)
    - ready for B0gL0z check-up

--- app.js
+++ app.js
@@ -1,127 +1,129 @@
-'use strict';
-
 const express = require('express');
 const mongoose = require('mongoose');
-const crypto = require('crypto');
-const passport = require('passport');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
+function handleError(err) {
+  return err;
+}
+
 mongoose.connect('mongodb://0.0.0.0:27017/ngram');
 
 const userSchema = new mongoose.Schema({
-    id: String,
-    fullName: String,
-    username: String,
-    hashedPassword: String
-  });
-
-const articleSchema = new mongoose.Schema({
-    id: String,
-    userid: String,
-    head: String,
-    body: String
+  id: String,
+  fullName: String,
+  username: String,
+  hashedPassword: String,
 });
 
-const user = mongoose.model('user', userSchema);
-const article = mongoose.model('article', articleSchema);
+const articleSchema = new mongoose.Schema({
+  id: String,
+  userid: String,
+  head: String,
+  body: String,
+});
+
+const User = mongoose.model('user', userSchema);
+const Article = mongoose.model('article', articleSchema);
 
 app.use(express.json());
 
-app.get("/",(req,res) => {
-    user.find({}, function (err, person) {
-        if (err) return handleError(err);
-        return res.status(200).send(person)})
-})
+app.get('/', (req, res) => {
+  User.find({}, (err, person) => {
+    if (err) return handleError(err);
+    return res.status(200).send(person);
+  });
+});
 
-app.get("/user/:id",(req,res) => {
-    const idRequest = req.params["id"];
+app.get('/user/:id', (req, res) => {
+  const idRequest = req.params.id;
 
-    user.find({id: idRequest}, function (err, person) {
-        if (err) return handleError(err);
-        return res.status(200).send(person)})
-})
+  User.find({ id: idRequest }, (err, person) => {
+    if (err) return handleError(err);
+    return res.status(200).send(person);
+  });
+});
 
-app.get("/article/:id",(req,res) => {
-    const idRequest = req.params["id"];
+app.get('/article/:id', (req, res) => {
+  const idRequest = req.params.id;
 
-    article.find({id: idRequest}, function (err, article) {
-        if (err) return handleError(err);
-        return res.status(200).send(article)})
-})
+  Article.find({ id: idRequest }, (err, articlePost) => {
+    if (err) return handleError(err);
+    return res.status(200).send(articlePost);
+  });
+});
 
-app.post("/user/add",(req, res) => {
-    const userRequest = req.body;
+app.post('/user/add', (req, res) => {
+  const userRequest = req.body;
+  const newUser = new User({
+    id: uuidv4(),
+    fullName: userRequest.fullName,
+    username: userRequest.username,
+    hashedPassword: userRequest.hashedPassword,
+  });
+  newUser.save((err) => {
+    if (err) return handleError(err);
+    return res.status(200).send('Done!');
+  });
+});
 
-    const newUser = new user ({
-        id: uuidv4(),
-        fullName: userRequest["fullName"],
-        username: userRequest["username"],
-        hashedPassword: userRequest["hashedPassword"]
-    });
+app.put('/user/:id/update', (req, res) => {
+  const userRequest = req.body;
+  const idRequest = req.params.id;
 
-    newUser.save(function (err) {
-        if (err) return handleError(err);
-        return res.status(200).send("Done!")})
-})
+  User.updateOne({ id: idRequest }, {
+    fullName: userRequest.fullName,
+    username: userRequest.username,
+    hashedPassword: userRequest.hashedPassword,
+  }, (err, person) => {
+    if (err) return handleError(err);
+    return res.status(200).send(person);
+  });
+});
 
-app.put("/user/:id/update", (req,res) => {
-    const userRequest = req.body;
-    const idRequest = req.params["id"];
+app.delete('/user/:id/delete', (req, res) => {
+  const idRequest = req.params.id;
 
-    user.updateOne({id: idRequest}, {
-        fullName: userRequest["fullName"],
-        username: userRequest["username"],
-        hashedPassword: userRequest["hashedPassword"]
-    }, function (err, person) {
-        if (err) return handleError(err);
-        return res.status(200).send(person)})
-})
+  User.deleteOne({ id: idRequest }, (err, person) => {
+    if (err) return handleError(err);
+    return res.status(200).send(person);
+  });
+});
 
-app.delete("/user/:id/delete", (req,res) => {
-    const idRequest = req.params["id"];
+app.post('/article/add', (req, res) => {
+  const userRequest = req.body;
+  const newArticle = new Article({
+    id: uuidv4(),
+    userid: userRequest.userid,
+    head: userRequest.head,
+    body: userRequest.body,
+  });
+  newArticle.save((err) => {
+    if (err) return handleError(err);
+    return res.status(200).send('Done!');
+  });
+});
 
-    user.deleteOne({id: idRequest}, function (err, person) {
-        if (err) return handleError(err);
-        return res.status(200).send(person)})
-})
+app.put('/article/:articleid/update', (req, res) => {
+  const userRequest = req.body;
+  const idRequest = req.params.articleid;
+  Article.updateOne({ id: idRequest }, {
+    userid: userRequest.userid,
+    head: userRequest.head,
+    body: userRequest.body,
+  }, (err, articlePost) => {
+    if (err) return handleError(err);
+    return res.status(200).send(articlePost);
+  });
+});
 
-app.post("/article/add", (req,res) => {
-    const userRequest = req.body;
-
-    const newArticle = new article ({
-        id: uuidv4(),
-        userid: userRequest["userid"],
-        head: userRequest["head"],
-        body: userRequest["body"]
-    });
-
-    newArticle.save(function (err) {
-        if (err) return handleError(err);
-        return res.status(200).send("Done!")})
-})
-
-app.put("/article/:articleid/update", (req,res) => {
-    const userRequest = req.body;
-    const idRequest = req.params["articleid"];
-
-    article.updateOne({id: idRequest}, {
-        userid: userRequest["userid"],
-        head: userRequest["head"],
-        body: userRequest["body"]
-    }, function (err, article) {
-        if (err) return handleError(err);
-        return res.status(200).send(article)})
-})
-
-app.delete("/article/:articleid/delete", (req,res) => {
-    const idRequest = req.params["articleid"];
-
-    user.deleteOne({id: idRequest}, function (err, article) {
-        if (err) return handleError(err);
-        return res.status(200).send(article)})
-})
-
+app.delete('/article/:articleid/delete', (req, res) => {
+  const idRequest = req.params.articleid;
+  Article.deleteOne({ id: idRequest }, (err, articlePost) => {
+    if (err) return handleError(err);
+    return res.status(200).send(articlePost);
+  });
+});
 
 app.listen(5000);
